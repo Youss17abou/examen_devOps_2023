90f2395f807a5f934e864b2f83d860b64956b004
Youssef <youssef.abouhamid@student.vinci.be>
2023-01-20 11:00:42 +0100

    test 1

--- src/services/__tests__/validations.test.js
+++ src/services/__tests__/validations.test.js
@@ -15,6 +15,16 @@ describe("validations tests suites - isEmpty", () => {
         const result = isEmpty("Label");
         expect(result).toBe(false);
     });
+
+    test("Il doit contenir au moins 8 caractères => return true", () => {
+        const result = tailleCorrecte("yotoyoto");
+        expect(result).toBe(true);
+    });
+    test("Il doit contenir au moins 8 caractères => return false", () => {
+        const result = tailleCorrecte("yoto");
+        expect(result).toBe(false);
+    });
+
 });
 
 // TODO: Create tests suite for validation function
--- src/services/validations.js
+++ src/services/validations.js
@@ -1,5 +1,7 @@
 // TODO: Add here the function to validate
 
 const isEmpty = (label) => !label || label.length === 0;
+const tailleCorrecte = (label) => !label || label.length === 9;
 
 exports.isEmpty = isEmpty;
+
